28705800ecf535c3e0ff8beb4d0e7c4bae7a3008
Christoph Honal <christoph.honal@web.de>
2024-07-09 14:49:41 +0200

    Simplify

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,22 @@
-#include <iostream>
-#include "MyClass.h"
+#include <cstdint>
+#include <cstdio>
 
-int main() {
-    MyClass myClass;
-    myClass.sayHello();
+#include "linux_nfc_api.h"
+
+
+static nfcTagCallback_t g_TagCB;
+
+int main()
+{   
+    // g_TagCB.onTagArrival = onTagArrival;
+    // g_TagCB.onTagDeparture = onTagDeparture;
+
+    nfcManager_doInitialize();
+    int fw = nfcManager_getFwVersion();
+    uint8_t rom_code_version = (fw >> 16) & 0xFF;
+    uint8_t major_version = (fw >> 8) & 0xFF;
+    uint8_t minor_version = fw & 0xFF;
+    printf("NFC hardware ROM: %d, FW: %d.%d", rom_code_version, major_version, minor_version);
+
     return 0;
 }
